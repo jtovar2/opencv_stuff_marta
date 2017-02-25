4e6cd3d36211fce90b3c9cae327850d7132f2ba1
Javier <jtovar2@student.gsu.edu>
2017-02-25 14:35:14 -0500

    better algo for detecting things

--- test_optflow3.py
+++ test_optflow3.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2
 import math
 import random
+import
 cap = cv2.VideoCapture('simul.MOV')
 # params for ShiTomasi corner detection
 feature_params = dict( maxCorners = 500,
@@ -23,9 +24,13 @@ print p0
 mask = np.zeros_like(old_frame)
 
 count_things_exited_on_left = 0
+last_exit_on_right = 0
 count_things_exited_on_right = 0
+last_exit_on_left = 0
 count_things_exited_on_top = 0
+last_exit_on_top = 0
 count_things_exited_on_bottom = 0
+last_exit_on_bottom = 0
 
 avg_distance = 0
 
@@ -47,53 +52,72 @@ while(1):
     # Select good points
     good_new = p1[st==1]
     good_old = p0[st==1]
+    points_to_remove = []
+    points_to_keep = []
     # draw the tracks
     if good_old.shape[0] != 0:
         for i,(new,old) in enumerate(zip(good_new,good_old)):
             a,b = new.ravel()
             c,d = old.ravel()
-            mask = cv2.line(mask, (a,b),(c,d), color[i].tolist(), 2)
-            frame = cv2.circle(frame,(a,b),5,color[i].tolist(),-1)
+            if int(a) == int(c) and int(b) == int(d):
+                points_to_remove.append(i)
+            else:
+                points_to_keep.append(i)
+                mask = cv2.line(mask, (a,b),(c,d), color[i].tolist(), 2)
+                frame = cv2.circle(frame,(a,b),5,color[i].tolist(),-1)
 
+
+
+
+        '''if len(points_to_remove) > .7*good_new.shape[0]:
+            print "no new points no no"
+
+            p0 = cv2.goodFeaturesToTrack(frame_gray, mask = None, **feature_params)
+            cv2.imshow('frame', frame)
+            print "looking for new points"
+            continue
+        else:'''
+
+
+        frame = cv2.circle(frame, (int(width),0), 10, color[0].tolist(), -1)
+        frame = cv2.circle(frame, (int(width), int(height)), 10, color[0].tolist(), -1)
+        frame = cv2.circle(frame, (0, int(height)), 10, color[0].tolist(), -1)
+        frame = cv2.circle(frame, (0,0), 10, color[0].tolist(), -1)
+        img = cv2.add(frame,mask)
+        cv2.imshow('frame',img)
+        k = cv2.waitKey(30) & 0xff
+        if k == 27:
+            break
+        # Now update the previous frame and previous points
+        old_gray = frame_gray.copy()
+        for point in new_pts:
+            x = point[0][0]
+            y = point[0][1]
+            if (width - x) == 1:
+                count_things_exited_on_right += 1
+                print "exit on the right " + str(count_things_exited_on_right)
+            elif x ==  1:
+                count_things_exited_on_left += 1
+                print "exit on the left " + str(count_things_exited_on_left)
+            if (height - y) == 1:
+                count_things_exited_on_top += 1
+                print "exit on the top " + str(count_things_exited_on_top)
+            elif y == 1:
+                count_things_exited_on_bottom += 1
+                print "exit on the bottom " + str(count_things_exited_on_bottom)
+            print "we on point"
+
+        print "wayymeent"
+        print "wayyment"
+        good_new = np.delete(good_new, points_to_remove, axis=0)
+    else:
+        cv2.imshow('frame', frame)
+
+    if good_new.shape[0] == 0:
+         p0 = cv2.goodFeaturesToTrack(frame_gray, mask = None, **feature_params)
 
     else:
-        print "no new points no no"
-        p0 = cv2.goodFeaturesToTrack(frame_gray, mask = None, **feature_params)
-        cv2.imshow('frame', frame)
-        print "looking for new points"
-        continue
+        p0 = good_new.reshape(-1,1,2)
 
-    frame = cv2.circle(frame, (int(width),0), 10, color[0].tolist(), -1)
-    frame = cv2.circle(frame, (int(width), int(height)), 10, color[0].tolist(), -1)
-    frame = cv2.circle(frame, (0, int(height)), 10, color[0].tolist(), -1)
-    frame = cv2.circle(frame, (0,0), 10, color[0].tolist(), -1)
-    img = cv2.add(frame,mask)
-    cv2.imshow('frame',img)
-    k = cv2.waitKey(30) & 0xff
-    if k == 27:
-        break
-    # Now update the previous frame and previous points
-    old_gray = frame_gray.copy()
-    for point in new_pts:
-        x = point[0][0]
-        y = point[0][1]
-        if (width - x) < 10:
-            print "exit on the right"
-            count_things_exited_on_right =+ 1
-        elif x< 10:
-            print "exit on the left"
-            count_things_exited_on_left =+ 1
-
-        elif (height - y) < 10:
-            print "exit on top"
-            count_things_exited_on_top =+ 1
-        elif y < 10:
-            print "exit on bottom"
-            count_things_exited_on_bottom =+ 1
-        print "we on point"
-
-    print "wayymeent"
-    print "wayyment"
-    p0 = good_new.reshape(-1,1,2)
 cv2.destroyAllWindows()
 cap.release()
